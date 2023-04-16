e0586ccdbf3d3b120b1f023b94a276e1bac57af1
Daniel Perez Efremova <daniel.perezefremova@tomtom.com>
2023-04-17 00:12:44 +0200

    Add steps to preprocess pipeline. Remove special chars and punctuation. Remove stopwords on names

--- src/poi_matching/pipelines/preprocessing/nodes.py
+++ src/poi_matching/pipelines/preprocessing/nodes.py
@@ -3,11 +3,14 @@ from poi_matching.utils.similarity import SimilarityModel
 import pandas as pd
 import numpy as np
 import logging 
-from pyspark.sql.functions import *
+from pyspark.sql.functions import col, lower, lit, split, array_join, translate
 import pyspark 
 import advertools as adv
 from functools import reduce
+from pyspark.ml.feature import StopWordsRemover, Tokenizer
 
+
+SPECIAL_CHARS = '!"#$%&\'()*+,-./:;<=>?@[\\]^_{|}~'
 
 log = logging.getLogger(__name__)
 
@@ -45,26 +48,91 @@ def make_features(df:pd.DataFrame, parameters: dict)->pd.DataFrame:
     return df[columns_to_keep]
 
 def transform_to_matching_datamodel(df: pyspark.sql.DataFrame,
-                                     parameters: dict):
-    
+                                     parameters: dict) -> pyspark.sql.DataFrame:
     
     for column, new_column in parameters['columns_map'].items():
-        df = df.withColumnRenamed(column, new_column['name']) \
-                .withColumn(new_column['name'], col(new_column['name']).cast(new_column['type']))
+
+        _name, _type = new_column['name'], new_column['type']
+        df = df.withColumnRenamed(column, _name) \
+                .withColumn(_name, col(_name).cast(_type))
 
     df = df.withColumn('run_id', lit(parameters['run_id']))
     return df
 
-def generate_bag_of_stopwords(list_of_languages: list):
+def remove_punctuation_and_special_chars_on_names(df: pyspark.sql.DataFrame,
+                                        parameters: dict) -> pyspark.sql.DataFrame:
+
+    for column in ['name_1', 'name_2']:
+        df=df.withColumn(column, translate(column, SPECIAL_CHARS, ''))
+
+    return df
+
+def transform_array_to_string(df: pyspark.sql.DataFrame,
+                            input_columns:list,
+                            output_columns:list,
+                            pattern:str) -> pyspark.sql.DataFrame:
+
+    columns_list = list(zip(input_columns, output_columns))
+    for column, new_column in columns_list:
+        df = df.withColumn(new_column, array_join(col(column), pattern))
+
+    return df
+
+def generate_bag_of_stopwords(list_of_languages: list) -> list:
     
     stopwords_dictionary = {language: adv.stopwords[language] for language in list_of_languages}
-    bag_of_stopwords = reduce(lambda x,y: x+y, stopwords_dictionary.values())
+    bag_of_stopwords = list(reduce(lambda x,y: x | y, stopwords_dictionary.values()))
 
     return bag_of_stopwords
 
-def remove_stopwords(df: pyspark.sql.DataFrame, parameters: dict):
+def remove_stopwords(df: pyspark.sql.DataFrame,
+                    input_columns:list,
+                    output_columns:list,
+                    stopwords: list) -> pyspark.sql.DataFrame:
+
+    remover = StopWordsRemover(stopWords=stopwords)
+    remover.setInputCols(input_columns)
+    remover.setOutputCols(output_columns)
+
+    df_transformed = remover.transform(df)
+
+    return df_transformed
+
+def tokenize(df: pyspark.sql.DataFrame,
+                    input_columns:list,
+                    output_columns:list) -> pyspark.sql.DataFrame:
+    
+    columns_list = list(zip(input_columns, output_columns))
+    tokenizer = Tokenizer()
+
+    for in_col, out_col in columns_list:
+        tokenizer.setInputCol(in_col)
+        tokenizer.setOutputCol(out_col)
+        df = tokenizer.transform(df)
+
+    return df
+    
+def remove_stopwords_on_names(df: pyspark.sql.DataFrame,
+                     parameters: dict) -> pyspark.sql.DataFrame:
+
+    df = tokenize(df, input_columns=['name_1', 'name_2'], output_columns=['name_1_tokenized', 'name_2_tokenized'])
 
     stopwords = generate_bag_of_stopwords(parameters['stopwords']['languages'])
+    enriched_stopwords = stopwords + parameters['stopwords']['additional_stopwords']
+
+    df_transformed = remove_stopwords(df,
+                                    input_columns=['name_1_tokenized', 'name_2_tokenized'],
+                                    output_columns=['name_1_stopwords_filtered', 'name_2_stopwords_filtered'],
+                                    stopwords=enriched_stopwords)
+    
+    df_processed = transform_array_to_string(df_transformed,
+                                            input_columns=['name_1_stopwords_filtered', 'name_2_stopwords_filtered'],
+                                            output_columns=['name_1_filtered', 'name_2_filtered'],
+                                            pattern=' ')
+    return df_processed
 
 
-    return stopwords
+
+
+
+
--- src/poi_matching/pipelines/preprocessing/pipeline.py
+++ src/poi_matching/pipelines/preprocessing/pipeline.py
@@ -1,6 +1,7 @@
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import make_features, \
-                    transform_to_matching_datamodel
+from .nodes import  transform_to_matching_datamodel, \
+                    remove_stopwords_on_names, \
+                    remove_punctuation_and_special_chars_on_names
 
 
 
@@ -13,6 +14,18 @@ def create_pipeline(**kwargs) -> Pipeline:
                 inputs=["foursquare_data", "parameters"],
                 outputs="foursquare_data_dm",
                 name="transform_to_matching_datamodel",
+            ),
+            node(
+                func=remove_punctuation_and_special_chars_on_names,
+                inputs=["foursquare_data_dm", "parameters"],
+                outputs="foursquare_data_rm_special_chars",
+                name="remove_punctuation_and_special_chars_on_names",
+            ),
+            node(
+                func=remove_stopwords_on_names,
+                inputs=["foursquare_data_rm_special_chars", "parameters"],
+                outputs="foursquare_data_rm_stopwords_on_names",
+                name="remove_stopwords_on_names",
             ),
         ]
     )
